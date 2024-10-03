c0a35b8b5a1f66739d008f482fc342fda93ba59f
Prince Kumar Sahu <princesahu2530@gmail.com>
2024-10-03 18:31:10 +0100

    model, controller, router for  user and product

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ import 'dotenv/config'
 import { connect } from 'mongoose'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 //App Config
 const app = express()
@@ -16,6 +18,11 @@ app.use(express.json())
 app.use(cors())
 
 // api endpoints
+
+app.use('/api/user', userRouter)
+
+app.use('/api/product',productRouter)
+
 app.get('/',(req,res)=>{
     res.send("API Working")
 })
