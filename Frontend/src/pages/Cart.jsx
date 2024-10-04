209b4a40cc61a1348fae957bbdcd079282dd270c
Prince Kumar Sahu <princesahu2530@gmail.com>
2024-10-05 00:04:27 +0100

    connect backend api with ecommerce website

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -9,23 +9,24 @@ const Cart = () => {
   const {products, currency, cartItems, updateQuantity, navigate} = useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
 
-  useEffect(()=>{
-    const tempData = [];
-    for(const items in cartItems){
-      for(const item in cartItems[items]){
-        if (cartItems[items][item] > 0) {
-          tempData.push({
-            _id: items,
-            size: item,
-            quantity: cartItems[items][item]
-          })
+  useEffect(() => {
+    if (products.length > 0) {
+      const tempData = [];
+      for (const items in cartItems) {
+        for (const item in cartItems[items]) {
+          if (cartItems[items][item] > 0) {
+            tempData.push({
+              _id: items,
+              size: item,
+              quantity: cartItems[items][item],
+            });
+          }
         }
       }
+      // console.log(tempData);
+      setCartData(tempData);
     }
-    // console.log(tempData);
-    setCartData(tempData);
-    
-  },[cartItems])
+  }, [cartItems, products]);
 
   return (
     <div className='border-t pt-14'>
